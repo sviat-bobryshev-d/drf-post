38839ec265ea7c499d68d07e94aac14811433252
Sviatoslav Bobryshev <s.bobryshev@tg.dunice.net>
2023-09-26 11:56:46 +0300

    added posts pagination

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["id"]
