3c826ff972bc503c2843940e483769008687a184
Sviatoslav Bobryshev <s.bobryshev@tg.dunice.net>
2023-09-26 15:11:06 +0300

    register Post in admin-panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from .models import Post
+from django.contrib import admin
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content")
+    list_display_links = ("id", "title")
+    search_fields = ("title", "content")
+
+
+admin.site.register(Post, PostAdmin)
