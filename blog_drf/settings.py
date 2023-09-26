cec2f2ddd4d85a7cd92fbfe00f956f75fc92d77b
Sviatoslav Bobryshev <s.bobryshev@tg.dunice.net>
2023-09-26 14:45:46 +0300

    used postgres instead of sqlite3

--- blog_drf/settings.py
+++ blog_drf/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import Config, RepositoryEnv
 
-config = Config(RepositoryEnv("blog_drf/.env"))
+config = Config(RepositoryEnv(".env"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "blog_drf.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USERNAME"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
